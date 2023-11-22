eb317f24e1937f53a6696ab8512dba3935f6d6ef
bartekkru100 <bartekkru100@gmail.com>
2023-11-22 23:36:27 +0100

    Did some training yesterday

--- src/App.java
+++ src/App.java
@@ -1,6 +1,8 @@
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.ListIterator;
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -12,9 +14,14 @@ public class App {
         names.push("Bartek");
         names.push("Arina");
         names.push("Kupa");
-        System.out.println(names.remove());
-        System.out.println(names.remove());
-        System.out.println(names.remove());
 
+        ListIterator<String> it = names.listIterator();
+        it.next();
+        it.next();
+        it.add("Susan");
+        for(String s: names)
+        {
+        System.out.println(s);
+        }
     }
 }
