62b11bdc23e83c9642f42871af52d05aafb50667
bartekkru100 <bartekkru100@gmail.com>
2023-11-22 01:40:09 +0100

    Did some practise

--- src/App.java
+++ src/App.java
@@ -1,5 +1,20 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args)
+    {
+        LinkedList<String> names = new LinkedList<String>();
+        names.push("Bartek");
+        names.push("Arina");
+        names.push("Kupa");
+        System.out.println(names.remove());
+        System.out.println(names.remove());
+        System.out.println(names.remove());
+
     }
 }
