528e66d1a0776695aa33f318b16787df86cd4e6c
bartekkru100 <bartekkru100@gmail.com>
2023-12-04 13:44:36 +0100

    Finished the tutorial

---
+++ Collections/src/App.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class App {
+    public static void main(String[] args) {
+        ArrayList l = new ArrayList();
+        l.add(1);
+        l.add("dsrgdtg");
+        System.out.println(l.toString());
+    }
+}
---
+++ Collections/src/Person.java
@@ -0,0 +1,8 @@
+public class Person {
+    public String email;
+    public Person(String email)
+    {
+        this.email = email;
+    }
+    
+}
